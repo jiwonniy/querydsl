09024020ea0c349771f50ee535dbaffe0cfea7bd
jiwonniy <jiwoni0707@gmail.com>
2024-12-08 18:04:43 +0900

    Feat : [section5] 프로젝션과 결과반환

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,8 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -16,6 +18,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
+import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.QTeam;
@@ -591,6 +596,166 @@ public class QuerydslBasicTest {
 
         for (String s : result){
             System.out.println("s = " + s);
+        }
+    }
+    /**
+     * 프로젝션 결과반환 - 기본
+     */
+    @Test
+    public void simpleProjection(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result){
+            System.out.println("s = " + s);
+        }
+    }
+
+    /**
+     * tuple로 조회
+     * 튜플은 레파지토리 계층에서까지만 사용하는게 좋아보인다.
+     * 서비스 계층이나 컨트롤러까지 넘어가는건 x
+     */
+    @Test
+    public void tupleProjection(){
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username=" + username); System.out.println("age=" + age);
+        }
+    }
+
+    /**
+     * 프로젝션과 결과 반환 - DTO 조회
+     * 순수 JPA에서 DTO 조회
+     * 1. 순수 JPA에서 DTO를 조회할 때는 new 명령어를 사용해야함
+     * 2. DTO의 package이름을 다 적어줘야해서 지저분함
+     * 3. 생성자 방식만 지원함
+     */
+    @Test
+    public void findDtoByJPQL(){
+        List<MemberDto> result = em.createQuery(
+                        "select new study.querydsl.dto.MemberDto(m.username, m.age) " +
+                                "from Member m", MemberDto.class)
+                .getResultList();
+        for (MemberDto memberDto : result){
+            System.out.println("MemberDto = " + memberDto);
+        }
+    }
+
+    /**
+     * Querydsl 빈 생성(Bean population)
+     * 결과를 DTO 반환할 때 사용
+     * 1. 프로퍼티 접근
+     * 2. 필드 직접 접근
+     * 3. 생성자 사용
+     */
+    @Test
+    public void findDtoBySetter(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result){
+            System.out.println("MemberDto = " + memberDto);
+        }
+    }
+
+    /**
+     * 2. 필드 직접 접근
+     */
+    @Test
+    public void findDtoByField(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result){
+            System.out.println("MemberDto = " + memberDto);
+        }
+    }
+
+    /**
+     * 2-1. 별칭이 다른 경우
+     */
+    @Test
+    public void findUserDtoByField(){
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username.as("name"),
+                        member.age))
+                .from(member)
+                .fetch();
+        for (UserDto userDto : result){
+            System.out.println("UserDto = " + userDto);
+        }
+    }
+
+    /**
+     * 2-1. age 를 받는 방법, 이름이 없을 때
+     */
+    @Test
+    public void findUserDto(){
+        QMember memberSub = new QMember("memberSub");
+
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        member.username.as("name"),
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(memberSub.age.max())
+                                        .from(memberSub), "age")))
+                .from(member)
+                .fetch();
+        for (UserDto userDto : result){
+            System.out.println("UserDto = " + userDto);
+        }
+    }
+
+
+
+
+    /**
+     * 3. 생성자 접근방법
+     */
+    @Test
+    public void findDtoByConstructor(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result){
+            System.out.println("MemberDto = " + memberDto);
+        }
+    }
+
+    /**
+     * 프로젝션과 결과 반환 - @QueryProjection
+     * 장점 : 컴파일러로 타입을 체크할 수 있으므로 가장 안전한 방법
+     * 단점 : DTO에 QueryDSL 어노테이션을 유지해야됨, DTO까지 Q 파일을 생성해야 하는 단점 존재
+     * (dto가 querydsl 에 의존성을 가짐)
+     */
+    @Test
+    public void findDtoByQueryProjection(){
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+        for (MemberDto memberDto : result){
+            System.out.println("MemberDto = " + memberDto);
         }
     }
 }
